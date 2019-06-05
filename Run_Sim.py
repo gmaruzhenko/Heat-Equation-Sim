9ca2252bc899b4e30a6e5a7cad6c6f178e730cfd
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-06-05 16:57:36 -0700

    done

--- Run_Sim.py
+++ Run_Sim.py
@@ -12,7 +12,6 @@ N = 20
 # Boundary conditions
 U1 = 1.
 U2 = 3.
-
 # Variables
 L = 3.
 K = 0.5
@@ -21,11 +20,14 @@ FN = 0
 # x range to work in
 START = 0
 END = L
+# Times to plot between
+T_INITIAL = 0.1
+T_FINAL = 2
+T_STEP = 0.1
 
-T_INITIAL = 0
-T_FINAL = 10
+# prep arrays
 x_axis = np.linspace(START, END)
-t_array = np.arange(T_INITIAL, T_FINAL)
+t_array = np.arange(T_INITIAL, T_FINAL, T_STEP)
 
 
 # Steady state solution
@@ -34,15 +36,17 @@ def w(x):
     return result
 
 
-# Creates fourier expansion for the transient solution given index n
+# Creates fourier expansion for the transient solution given a time and x domain
 def v(t, x_values):
-    # for one x and t
+    # find value for the positional component
     x_step = list(map(get_x_value, x_values))
-    total = list(map(lambda x: x * get_t_values(t), x_step))
+    # multiply positional component by the decay function
+    decay_component = get_t_value(t)
+    transient_solution = list(map(lambda x: x * decay_component, x_step))
+    return transient_solution
 
-    print(total)
-    return total
 
+# Get position component with number of terms N
 def get_x_value(x):
     n_terms = np.arange(0, N)
     terms = np.ones(len(n_terms))
@@ -53,7 +57,8 @@ def get_x_value(x):
     return sum(terms)
 
 
-def get_t_values(t):
+# Get decay component with number of terms N
+def get_t_value(t):
     n_terms = np.arange(0, N)
     terms = np.ones(len(n_terms))
     count = 0
@@ -63,8 +68,7 @@ def get_t_values(t):
     return sum(terms)
 
 
-
-# solves intergation to get constant for a given n
+# solves integration to get constant for a given n
 def get_bn(n):
     integral = quad(bn_integrand, 0, L, args=(n))
     return -2. / L * integral[0]
@@ -75,15 +79,27 @@ def bn_integrand(x, n):
     return w(x) * sin(n * pi * x / L)
 
 
+# plot one time for the domain
 def plot_one_timestep(t):
     plt.plot(x_axis, v(t, x_axis) + w(x_axis))
 
 
-#I =  quad(bn_integrand, 0, L, args=(1))
+# Show and label graph
+def show_plots():
+    plt.title('Heat Equation Solutions')
+    plt.xlabel('x')
+    plt.ylabel('u(x,t)')
+    # labeled just a few timestamps
+    plt.legend(
+        ['Steady State Solution w(x)',
+         't = ' + str(t_array[0]),
+         't = ' + str(t_array[1]),
+         't = ' + str("%.1f" % t_array[2])])
+    plt.show()
 
-# print(I)
-# print(get_bn(5))
+# Run plotting functions
+plt.plot(x_axis, w(x_axis), 'r')
 [plot_one_timestep(t) for t in t_array]
 
-# plt.plot(x_axis, v(1,x_axis))
-plt.show()
+show_plots()
+
