a959edf53099e496101db91e5e4b0989fe1e69bd
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-06-05 14:02:54 -0700

    graphing steady and transient solution

--- Run_Sim.py
+++ Run_Sim.py
@@ -8,21 +8,25 @@ from scipy.integrate import quad
 import math as math
 
 # Number of terms for fourier expansion
-N = 10
+N = 20
 # Boundary conditions
 U1 = 1.
 U2 = 3.
+
 # Variables
 L = 3.
 K = 0.5
 # Initial Condition
 FN = 0
+# x range to work in
+START = 0
+END = L
 
 T_INITIAL = 0
-x_axis = np.linspace(0, L)
-t_array = np.linspace(T_INITIAL, 10)
+T_FINAL = 10
+x_axis = np.linspace(START, END)
+t_array = np.arange(T_INITIAL, T_FINAL)
 
-v_terms = np.full(N, map(lambda n: v(), range(0,N-1,1)))
 
 # Steady state solution
 def w(x):
@@ -31,8 +35,33 @@ def w(x):
 
 
 # Creates fourier expansion for the transient solution given index n
-def v(x, t, n):
-    return get_bn(n) * sin(n * pi * x / L) * e**((n * pi / L)**2 * t)
+def v(t, x_values):
+    # for one x and t
+    x_step = list(map(get_x_value, x_values))
+    total = list(map(lambda x: x * get_t_values(t), x_step))
+
+    print(total)
+    return total
+
+def get_x_value(x):
+    n_terms = np.arange(0, N)
+    terms = np.ones(len(n_terms))
+    count = 0
+    while(count < N):
+        terms[count] = get_bn(n_terms[count]) * sin(n_terms[count] * pi * x / L)
+        count += 1
+    return sum(terms)
+
+
+def get_t_values(t):
+    n_terms = np.arange(0, N)
+    terms = np.ones(len(n_terms))
+    count = 0
+    while (count < N):
+        terms[count] = e**(-1 * (n_terms[count] * pi / L)**2 * t)
+        count += 1
+    return sum(terms)
+
 
 
 # solves intergation to get constant for a given n
@@ -46,10 +75,15 @@ def bn_integrand(x, n):
     return w(x) * sin(n * pi * x / L)
 
 
+def plot_one_timestep(t):
+    plt.plot(x_axis, v(t, x_axis) + w(x_axis))
+
+
 #I =  quad(bn_integrand, 0, L, args=(1))
 
 # print(I)
-print(get_bn(5))
-plt.plot(x_axis, w(x_axis))
-plt.plot(x_axis, v(x_axis,0,1), 'red')
+# print(get_bn(5))
+[plot_one_timestep(t) for t in t_array]
+
+# plt.plot(x_axis, v(1,x_axis))
 plt.show()
